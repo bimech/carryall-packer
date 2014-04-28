e17e8cd573e4456f47599d961b1e9b4553322a44
Frank O'Hara <frankj.ohara@gmail.com>
2014-04-28 15:52:02 -0400

    Prevent main from auto-running; expose packer for API usage.

--- bin/carryall-packer.js
+++ bin/carryall-packer.js
@@ -2,4 +2,5 @@
 var path = require('path');
 var fs = require('fs');
 var lib = path.join(path.dirname(fs.realpathSync(__filename)), '../lib');
-require(lib + '/main.js');
+var main = require(lib + '/main.js');
+main.run();
